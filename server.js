bfe027ae244d091c79b408ae237cf6962b784027
vitor manoel <vitormanoeldev13@gmail.com>
2026-02-27 14:21:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Conectado ao MongoDB 🚀"))
+  .catch(err => console.log("Erro ao conectar:", err));
 // Permitir receber dados do formulário
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,17 +15,42 @@ app.use(express.json());
 // Servir arquivos estáticos (HTML, CSS)
 app.use(express.static(path.join(__dirname, 'public')));
 
+const Agendamento = mongoose.model('Agendamento', {
+  nome: String,
+  telefone: String,
+  data: String,
+  time: String,
+  servico: String,
+  criadoEm: {
+    type: Date,
+    default: Date.now
+  }
+});
+
 // Rota POST
-app.post('/agendar', (req, res) => {
+app.post('/agendar', async (req, res) => {
+  try {
     const { nome, telefone, data, time, servico } = req.body;
 
-    console.log("Novo agendamento:");
-    console.log(nome, telefone, data, time, servico);
+    const novoAgendamento = new Agendamento({
+      nome,
+      telefone,
+      data,
+      time,
+      servico
+    });
 
-    res.send("Agendamento recebido com sucesso!");
+    await novoAgendamento.save();
+
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    res.send("Erro ao salvar agendamento.");
+  }
 });
 
 // Iniciar servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
+
 });
